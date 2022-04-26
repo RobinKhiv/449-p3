edfbe25c57faa75ed8cc229559110a358bcd6035
robinkhiv <robinkhiv@gmail.com>
2022-04-26 01:28:44 -0700

    added service 1

--- stats.py
+++ stats.py
@@ -22,6 +22,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
 
+
 def get_db():
     with contextlib.closing(sqlite3.connect(settings.database)) as db:
         db.row_factory = sqlite3.Row
@@ -38,6 +39,52 @@ app: FastAPI = FastAPI()
 logging.config.fileConfig(settings.logging_config)
 
 
+def check_for_game(game: int, id: int, db: sqlite3.Connection):
+    try:
+        cur = db.execute("select exists(SELECT * from games WHERE user_id = ? AND game_id = ? LIMIT 1);", (id, game))
+        query = cur.fetchone()[0]
+        return query
+    except sqlite3.IntegrityError as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail={"type": type(e).__name__, "msg": str(e)},
+        )
+
+
+def post_game_service(win:bool, guesses: int, user_id: int, game_id:int, db: sqlite3.Connection):
+    try:
+        cur = db.execute("INSERT into games (user_id, game_id, guesses, won) VALUES (?, ?, ?,?);",(user_id, game_id,guesses,win))
+        db.commit()
+        return
+    except sqlite3.IntegrityError as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail={"type": type(e).__name__, "msg": str(e)},
+        )
+
+
+def update_game_service(win:bool, guesses: int, user_id:int, game_id:int, db: sqlite3.Connection):
+    try:
+        cur = db.execute("UPDATE games SET guesses = ?, won = ? WHERE user_id = ? AND game_id = ?;", (guesses,win,user_id,game_id))
+        db.commit()
+    except sqlite3.IntegrityError as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail={"type": type(e).__name__, "msg": str(e)},
+        )
+    return
+
+
+#Posting a win or loss for a particular game, along with a timestamp and number of guesses
+@app.post("/stats/")
+def post_stats_by_userid(win: bool, guesses: int, user_id: int, game_id: int, db: sqlite3.Connection = Depends(get_db)):
+    if check_for_game(game_id, user_id, db):
+        update_game_service(win, guesses, user_id, game_id, db)
+    else:
+        post_game_service(win, guesses, user_id, game_id, db)
+    return "game posted";
+
+
 # top 10 users by number of wins
 @app.post("/stats/top10wins")
 def get_top10users(
@@ -51,6 +98,7 @@ def get_top10users(
         i=i+1
         dicts[i] = row["user_id"]
     return {"Top 10 users by number of wins are": dicts}
+
 
 # top 10 users by longest streak
 @app.post("/stats/top10streaks")
